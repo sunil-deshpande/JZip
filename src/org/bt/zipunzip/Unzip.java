165c2018b97541a179bf02e1149c3f2b1c08beee
Sunil S. Deshpande <hsdesh@yahoo.com>
2016-06-26 07:09:29 +0530

    Indentation corrected. Tabs replaced with two spaces.

--- src/org/bt/zipunzip/Unzip.java
+++ src/org/bt/zipunzip/Unzip.java
@@ -9,68 +9,68 @@ import java.text.SimpleDateFormat;
 import java.util.zip.*;
 
 public class Unzip {
-		String fileSeparator = System.getProperty("file.separator");
-		String zipFile = null;
-		String destPath = null;
-		private final byte[] BUFFER = new byte[4096 * 1024];
-		public Unzip(String zipFile, String destPath)
-		{
-			this.zipFile = zipFile;
-            if (destPath != null) {
-    			this.destPath = destPath;
-            } else {
-                this.destPath = "";
-            }
+	String fileSeparator = System.getProperty("file.separator");
+	String zipFile = null;
+	String destPath = null;
+	private final byte[] BUFFER = new byte[4096 * 1024];
+	public Unzip(String zipFile, String destPath)
+	{
+		this.zipFile = zipFile;
+		if (destPath != null) {
+			this.destPath = destPath;
+		} else {
+			this.destPath = "";
+		}
 
-		}
-		public void unZipFile() throws ZipException, IOException
-		{
-			ZipEntry zEntry;
-			FileOutputStream fo;
-			int bytesRead;
-			
-			FileInputStream fi = new FileInputStream(zipFile);
-			ZipInputStream zi = new ZipInputStream(fi);
-			while((zEntry = zi.getNextEntry()) != null) {
-				if (zEntry.getMethod() == ZipEntry.DEFLATED) {
-					System.out.println("Deflating file: " + destPath + zEntry.getName());
-					fo = new FileOutputStream(destPath + zEntry.getName());
-					
-					while ((bytesRead = zi.read(BUFFER)) != -1) {
-							fo.write(BUFFER, 0, bytesRead);
-					}
-					fo.close();
-					zi.closeEntry();
+	}
+	public void unZipFile() throws ZipException, IOException
+	{
+		ZipEntry zEntry;
+		FileOutputStream fo;
+		int bytesRead;
+
+		FileInputStream fi = new FileInputStream(zipFile);
+		ZipInputStream zi = new ZipInputStream(fi);
+		while((zEntry = zi.getNextEntry()) != null) {
+			if (zEntry.getMethod() == ZipEntry.DEFLATED) {
+				System.out.println("Deflating file: " + destPath + zEntry.getName());
+				fo = new FileOutputStream(destPath + zEntry.getName());
+
+				while ((bytesRead = zi.read(BUFFER)) != -1) {
+					fo.write(BUFFER, 0, bytesRead);
 				}
+				fo.close();
+				zi.closeEntry();
 			}
-			zi.close();
 		}
-        public void listFiles() throws ZipException, IOException
-        {
-			ZipEntry zEntry;
-			int bytesRead;
-            String method = "";
-            int count = 0;
-            long totalSize = 0;
-			
-			FileInputStream fi = new FileInputStream(zipFile);
-			ZipInputStream zi = new ZipInputStream(fi);
-			System.out.printf("%15s %12s %10s %s\n", "Size","Date", "Method", "File Name");
-			System.out.printf("%15s %12s %10s %s\n", "-------------","-----------", "---------", "-------");
-			while((zEntry = zi.getNextEntry()) != null) {
-                count++;
-                totalSize += zEntry.getSize();
-				if (zEntry.getMethod() == ZipEntry.DEFLATED) method = "Deflated";
-				if (zEntry.getMethod() == ZipEntry.STORED) method = "Stored";
-                if (!zEntry.isDirectory()) {
-				    System.out.printf("%15d %12s %-10s %s\n", zEntry.getSize(), new SimpleDateFormat("MM-dd-yyyy").format(new java.util.Date(zEntry.getTime())), method, destPath + zEntry.getName());
-                } else {
-                    System.out.printf("%15s %12s %-10s %s\n", "<DIR>", new SimpleDateFormat("MM-dd-yyyy").format(new java.util.Date(zEntry.getTime())), method, destPath + zEntry.getName());
-                }
-			    zi.closeEntry();
+		zi.close();
+	}
+	public void listFiles() throws ZipException, IOException
+	{
+		ZipEntry zEntry;
+		int bytesRead;
+		String method = "";
+		int count = 0;
+		long totalSize = 0;
+
+		FileInputStream fi = new FileInputStream(zipFile);
+		ZipInputStream zi = new ZipInputStream(fi);
+		System.out.printf("%15s %12s %10s %s\n", "Size","Date", "Method", "File Name");
+		System.out.printf("%15s %12s %10s %s\n", "-------------","-----------", "---------", "-------");
+		while((zEntry = zi.getNextEntry()) != null) {
+			count++;
+			totalSize += zEntry.getSize();
+			if (zEntry.getMethod() == ZipEntry.DEFLATED) method = "Deflated";
+			if (zEntry.getMethod() == ZipEntry.STORED) method = "Stored";
+			if (!zEntry.isDirectory()) {
+				System.out.printf("%15d %12s %-10s %s\n", zEntry.getSize(), new SimpleDateFormat("MM-dd-yyyy").format(new java.util.Date(zEntry.getTime())), method, destPath + zEntry.getName());
+			} else {
+				System.out.printf("%15s %12s %-10s %s\n", "<DIR>", new SimpleDateFormat("MM-dd-yyyy").format(new java.util.Date(zEntry.getTime())), method, destPath + zEntry.getName());
 			}
-			System.out.printf("%15s %12s %10s %s\n", "--------------","", "", "-------");
-			System.out.printf("%15d %12s %10s %d files\n", totalSize,"", "", count);
-			zi.close();
+			zi.closeEntry();
 		}
+		System.out.printf("%15s %12s %10s %s\n", "--------------","", "", "-------");
+		System.out.printf("%15d %12s %10s %d files\n", totalSize,"", "", count);
+		zi.close();
+	}
 }
